94ca7c91f00347413d1fef2f9959cdabd2864fea
kingapl <kingapluciennik@wp.pl>
2020-12-30 20:43:01 +0100

    handling exceptions

--- guess_the_number/guess_the_number.py
+++ guess_the_number/guess_the_number.py
@@ -5,12 +5,18 @@ secret_number = random.randint(1, 100)
 print("Myślę o pewnej liczbie z zakresu 1-100.")
 number = 0
 
-while number != secret_number:
-    number = int(input("Zgadnij jaka to liczba.\n"))
+while True:
+    try:
+        while number != secret_number:
+    
+            number = int(input("Zgadnij jaka to liczba.\n"))
 
-    if number == secret_number:
-        print("Tak! To właśnie ta liczba. Gratulacje!")
-    elif number < secret_number:
-        print(f"Liczba, o której myślę jest większa niż {number}.\n")
-    elif number > secret_number:
-        print(f"Liczba, o której myślę jest mniejsza niż {number}.\n")
+            if number == secret_number:
+                print("Tak! To właśnie ta liczba. Gratulacje!")
+            elif number < secret_number:
+                print(f"Liczba, o której myślę jest większa niż {number}.\n")
+            elif number > secret_number:
+                print(f"Liczba, o której myślę jest mniejsza niż {number}.\n")
+        break
+    except ValueError:
+        print("Nie wpisano liczby!")
