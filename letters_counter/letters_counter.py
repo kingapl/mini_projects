84bcd27424ce046d48e377c04d1c1d38ffe1d6ef
kingapl <kingapluciennik@wp.pl>
2021-01-21 18:29:50 +0100

    Added the function of counting letters appearing in the text.

--- letters_counter/letters_counter.py
+++ letters_counter/letters_counter.py
@@ -1,16 +1,35 @@
 text = input("Wpisz tekst: ")
-letter = input("Którą literę policzyć? ")
-
-def letters_counter(letter):
-    counter = 0
-
-    for t in text.lower():
-        if t == letter.lower():
-            counter += 1
-
-    return counter
 
 
-counter = letters_counter(letter)
+def letters_in_text(text):
+    number_of_letters = 0
 
-print(f"\nLitera '{letter}' wystąpiła {counter} razy w tekście.")
+    for letter in text.lower():
+        if letter != ' ':
+            number_of_letters += 1
+
+    return number_of_letters
+
+
+def letter_frequency(text):
+    letters = {}
+
+    for letter in text.lower():
+        if letter == ' ':
+            continue
+        elif letter not in letters:
+            letters[letter] = 1
+        else:
+            letters[letter] += 1
+
+    return letters
+
+
+number_of_letters = letters_in_text(text)
+letters = letter_frequency(text)
+
+print(f"\nTekst zawiera {number_of_letters} liter.\n")
+print("Częstotliwość występowania liter:")
+
+for key, value in letters.items():
+    print(f"{key.upper()} - {value}")
