c091bb222e432a4b21d5cd198e647e4739a05fa5
kingapl <kingapluciennik@wp.pl>
2021-03-02 11:46:57 +0100

    Add function

--- cats_facts/cats_facts.py
+++ cats_facts/cats_facts.py
@@ -1,12 +1,17 @@
 import requests
 from random import choice
 
-print("Cat fact:")
 
-url = 'https://cat-fact.herokuapp.com/facts/'
-response = requests.get(url)
-response_dict = response.json()
+def get_cat_fact():
+    url = 'https://cat-fact.herokuapp.com/facts/'
+    response = requests.get(url)
+    response_dict = response.json()
 
-fact = choice(response_dict)
-cat_fact = fact['text']
-print(cat_fact)
+    fact = choice(response_dict)
+    cat_fact = fact['text']
+    
+    return cat_fact
+
+
+cat_fact = get_cat_fact()
+print(f"Cat fact: {cat_fact}")
