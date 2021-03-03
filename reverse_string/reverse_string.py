c173d1c94573354f9ab5a2cba065d870edd205a7
kingapl <kingapluciennik@wp.pl>
2021-03-03 14:14:32 +0100

    Remove function in reverse_string.py.

--- reverse_string/reverse_string.py
+++ reverse_string/reverse_string.py
@@ -1,13 +1,2 @@
 string = input("Wpisz tekst: ")
-
-def reverse(string):
-    reverse_string = ""
-
-    for s in string:
-        reverse_string = s + reverse_string
-
-    return reverse_string
-
-reverse_string = reverse(string)
-
-print(f"Odwrócony ciąg: {reverse_string}.")
+print(f"Odwrócony ciąg: {string[::-1]}.")
