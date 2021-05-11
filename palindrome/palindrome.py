5394cdad241c41345332536cd488762656319321
kingapl <kingapluciennik@wp.pl>
2021-05-11 23:24:52 +0200

    Refactoring the palindrome.py file.

--- palindrome/palindrome.py
+++ palindrome/palindrome.py
@@ -1,5 +1,8 @@
-print("Sprawdź, czy słowo to palindrom.")
-text = input("Wpisz słowo: ")
+def check_if_palindrome(text: str):
+    if text.lower() == text[::-1].lower():
+        return f'{text} to palindrom.'
+    return f'{text} nie jest palindromem.'
 
-(print(f"{text.capitalize()} to palindrom.") if text.lower() == text[::-1].lower() 
-    else print(f"{text.capitalize()} to nie palindrom."))
+
+print("Sprawdź, czy słowo to palindrom.")
+print(check_if_palindrome(input("Wpisz słowo: ")))
