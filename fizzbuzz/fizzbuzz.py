83969b9eec34f4dc733400a5c967991122f670be
kingapl <kingapluciennik@wp.pl>
2021-05-11 23:31:41 +0200

    Refactoring the fizzbuzz.py file.

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,9 +1,13 @@
-for number in range(1, 101):
-    if number%15 == 0:
-        print("FizzBuzz")
-    elif number%3 == 0:
-        print("Fizz")
-    elif number%5 == 0:
-        print("Buzz")
-    else:
-        print(number)
+def fizzbuzz(limit):
+    for number in range(1, limit+1):
+        if number % 15 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+
+fizzbuzz(int(input('Podaj liczbę: ')))
